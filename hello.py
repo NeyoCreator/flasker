f8ff2cf343811a506b0ce673bfd5079a14c70b1d
NeyoCreator <neos25722@gmail.com>
2022-01-18 23:07:59 +0200

    Added Post Page

--- hello.py
+++ hello.py
@@ -1,39 +1,41 @@
+#Imports
 from enum import unique
 from flask import Flask,render_template,flash,request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,PasswordField, BooleanField, ValidationError
 from wtforms import validators
-from  wtforms.validators import DataRequired, EqualTo, Length
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms.widgets import TextArea
 
 
-# Create a Flask Intsance
+#Flask Intsance
 app = Flask(__name__)
 
 #Add to database SQLITE
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'\
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-#MySql
+#MYSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:BOSS97kills!@localhost/new_users'
 
 #Secrete Key!
 app.config['SECRET_KEY'] = 'any secret string'
 
-#Initilise the database
+#Initilise  Database
 db = SQLAlchemy(app)
 migrate=Migrate(app,db)
 
-#Create a model
+#User Model
 class Users(db.Model):
     id =  db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(200), nullable = False)
     email = db.Column(db.String(120), nullable = False, unique=True)
     favourite_color=db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default= datetime.utcnow)
-    #password
+    #Password
     password_hash=db.Column(db.String(128))
 
     @property
@@ -47,11 +49,19 @@ class Users(db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
     
-    #Create A String
     def __rep__(self):
         return '<Name %r>' % self.name
 
-#Create a Form Class
+#Blog Model
+class Posts(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(255))
+    content=db.Column(db.Text)
+    author=db.Column(db.String(255))
+    date_posted=db.Column(db.DateTime, default=datetime.utcnow)
+    slug=db.Column(db.String(255))
+
+#User Form Class
 class UserForm(FlaskForm):
     name=StringField("Name",validators=[DataRequired()])
     email=StringField("E-mail",validators=[DataRequired()])
@@ -60,11 +70,44 @@ class UserForm(FlaskForm):
     password_hash2=PasswordField("Confirm Paword",validators=[DataRequired()])
     submit = SubmitField("submit")
     
-#Form Class
+#Namer Form Class
 class NamerForm(FlaskForm):
     name=StringField("Whats your name",validators=[DataRequired()])
     submit=SubmitField("Submit")
 
+#Blog Form
+class PostForm(FlaskForm):
+    title=StringField("Title",validators=[DataRequired()])
+    content=StringField("Content",validators=[DataRequired()],widget=TextArea())
+    author=StringField("Author",validators=[DataRequired()])
+    slug=StringField("SlugFied",validators=[DataRequired()])
+    submit=SubmitField("Submit")
+
+#Post Page
+@app.route('/posts')
+def posts():
+    #Grab Posts
+    posts=Posts.query.order_by(Posts.date_posted)
+    return render_template("posts.html",posts=posts)
+
+#Add Post
+@app.route('/add-post',methods=['GET','POST'])
+def add_post():
+    form=PostForm()
+    if form.validate_on_submit():
+        post=Posts(title=form.title.data,content=form.content.data,author=form.author.data,slug=form.slug.data)
+        form.title.data=''
+        form.content.data=''
+        form.author.data=''
+        form.slug.data=''
+        #Add Database
+        db.session.add(post)
+        db.session.commit()
+        flash("Data Added successfully!")
+        
+    return render_template("add_post.html",form=form)
+
+        
 #Delete Database Record
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -100,21 +143,16 @@ def update(id):
     else:
         return render_template('update.html',form=form,name_to_update=name_to_update,id=id)
     
-
-#Create a route Decorator
+#Home Page
 @app.route('/')
 def index():
     first_name = 'Neo'
     stuff = 'This is <strong>Bold</strong> Text'
 
     favourite_pizza= ["peperonni", "cheese","mushroom",41]
-    return render_template("index.html", 
-    first_name =first_name,
-    stuff =stuff,
-    favourite_pizza = favourite_pizza
-    )
+    return render_template("index.html",first_name =first_name,stuff =stuff,favourite_pizza = favourite_pizza)
 
-#adding a user
+#Add User
 @app.route('/user/add',methods=['GET','POST'])
 def add_user():
     name = None
@@ -122,7 +160,7 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()  
         if user is None:
-            #Hash the password!
+            #Hash Password
             hashed_pw=generate_password_hash(form.password_hash.data, "sha256")
             user = Users(name=form.name.data, email=form.email.data,favourite_color=form.favourite_color.data,password_hash=hashed_pw) 
             db.session.add(user)
@@ -138,15 +176,12 @@ def add_user():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name =name )
-    #second commit
+    return render_template('user.html',name =name )
 
-#Bank_statement
+#Bank Statement
 @app.route('/bank')
 def bank():
     return render_template('bank_statement.html')
-
-#Create custom error pages
 
 #Invalid URL 
 @app.errorhandler(404) 
@@ -154,18 +189,18 @@ def page_not_found(e):
     return render_template("404.html"),404
 
 
-#Internal Servererro 
+#Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"),500
 
-#Create  a Name Page
+#Namer Page
 @app.route('/name',methods=['GET','POST'])
 def name():
     name = None
     form = NamerForm()
 
-    # Validate Form
+    #Validate Form
     if form.validate_on_submit():
         name = form.name.data
         flash(form.name.data +" your name has been submited Succefully!")
